d2ac4fc8543b96e9f474586f8a807db45d524914
gg <andrew.peter.bryan@gmail.com>
2019-07-03 20:18:04 -0500

    testing delphi oracle

--- contracts/testrick.sh
+++ contracts/testrick.sh
@@ -1,71 +1,5 @@
 #!/bin/bash
 # Usage: ./test.sh
-#eosio
-sudo apt remove eosio
-wget https://github.com/eosio/eos/releases/download/v1.7.4/eosio_1.7.4-1-ubuntu-18.04_amd64.deb
-sudo apt install ./eosio_1.7.4-1-ubuntu-18.04_amd64.deb
-
-git clone --recursive https://github.com/EOSIO/eos.git --branch v1.7.4 --single-branch
-cd scripts
-#Could not find a package configuration file provided by "libbsoncxx" with
-# eos/plugins/mongo_db_plugin/CMakeLists.txt find_package(libbsoncxx-static REQUIRED) -> find_package(libbsoncxx REQUIRED)
-# eos/plugins/mongo_db_plugin/CMakeLists.txt : if (LIBMONGOC-1.0_FOUND) -> if (LIBMONGOC_FOUND)
-# eos/scripts/eosio_build.sh --> -DBUILD_MONGO_DB_PLUGIN=false 
-./eosio_build.sh
-sudo ./eosio_install.sh
-
-#put into .bashrc
-alias nodeos=~/opt/eosio/bin/nodeos
-alias cleos=~/opt/eosio/bin/cleos
-alias keosd=~/opt/eosio/bin/keosd
-source ~/.bashrc
-
-
-#Could not find a package configuration file provided by "libbsoncxx" with
-wget https://github.com/mongodb/mongo-c-driver/releases/download/1.14.0/mongo-c-driver-1.14.0.tar.gz
-tar xzf mongo-c-driver-1.14.0.tar.gz
-cd mongo-c-driver-1.14.0
-mkdir cmake-build
-cd cmake-build
-cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
-
-git clone https://github.com/mongodb/mongo-cxx-driver.git --branch r3.4.0 --single-branch
---branch releases/stable --depth 1
-cd mongo-cxx-driver/build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local ..
-make -j$( sysctl -in machdep.cpu.core_count )
-sudo make install
-
-#boost 1.67
-git clone --recursive https://github.com/boostorg/boost.git --branch boost-1.67.0 --single-branch
-cd boost
-./bootstrap.sh
-sudo ./b2 install
- cat /usr/include/boost/version.hpp | grep "BOOST_LIB_VERSION"
- cat /usr/local/include/boost/version.hpp | grep "BOOST_LIB_VERSION"
- sudo add-apt-repository ppa:mhier/libboost-latest
-sudo apt update
-sudo apt install libboost1.67
-sudo ldconfig
-##############################################
-
-#eosio.contracts
-git clone --recursive https://github.com/EOSIO/eosio.contracts.git --branch v1.6.1 --single-branch
-git tag -l
-git checkout tags/v1.6.1
-git show
-git log -1
-
-#eosio-cdt
-sudo apt remove eosio.cdt
-wget https://github.com/EOSIO/eosio.cdt/releases/download/v1.5.0/eosio.cdt_1.5.0-1_amd64.deb
-sudo apt install ./eosio.cdt_1.5.0-1_amd64.deb
-#eosio-cdt
-git clone --recursive https://github.com/eosio/eosio.cdt --branch v1.5.0 --single-branch
-cd eosio.cdt
-./build.sh
-sudo ./install.sh
-##############################################
 #=================================================================================#
 # SETUP
 # 
@@ -132,7 +66,6 @@ echo -e "${CYAN}-----------------------SYSTEM CONTRACTS-----------------------${
 #eosio-cpp -contract=eosio.wrap -I=$EOSIO_CONTRACTS_ROOT/eosio.wrap/include -o=$EOSIO_CONTRACTS_ROOT/eosio.wrap/eosio.wrap.wasm -abigen $EOSIO_CONTRACTS_ROOT/eosio.wrap/src/eosio.wrap.cpp
 #eosio-cpp -contract=eosio.token -I=$EOSIO_CONTRACTS_ROOT/eosio.token/include -o=$EOSIO_CONTRACTS_ROOT/eosio.token/eosio.token.wasm -abigen $EOSIO_CONTRACTS_ROOT/eosio.token/src/eosio.token.cpp
 #eosio-cpp -contract=eosio.bios -I=$EOSIO_CONTRACTS_ROOT/eosio.bios/include -o=$EOSIO_CONTRACTS_ROOT/eosio.bios/eosio.bios.wasm -abigen $EOSIO_CONTRACTS_ROOT/eosio.bios/src/eosio.bios.cpp
-cleos system newaccount eosio --transfer accountnum11 EOS8mUftJXepGzdQ2TaCduNuSPAfXJHf22uex4u41ab1EVv9EAhWt --stake-net "100000.0000 SYS" --stake-cpu "100000.0000 SYS" --buy-ram-kbytes 8192
 
 cleos set contract eosio.token $EOSIO_CONTRACTS_ROOT/eosio.token/
 cleos set contract eosio.msig $EOSIO_CONTRACTS_ROOT/eosio.msig
@@ -240,11 +173,12 @@ ORACLE="oracle"
 ORACLE_WASM="$ORACLE.wasm"
 ORACLE_ABI="$ORACLE.abi"
 ORACLE_CPP="$ORACLE.cpp"
-eosio-cpp --abigen -I $ORACLE_ROOT -I $EOSIO_CONTRACTS_ROOT/eosio.system/include -o "$ORACLE_ROOT/$ORACLE_WASM" "$ORACLE_ROOT/$ORACLE_CPP" 
+EOSIO_CONTRACTS_ROOT=/home/gg/contracts/eosio.contracts/contracts
+eosio-cpp -contract=$ORACLE -I=$ORACLE_ROOT -I=$EOSIO_CONTRACTS_ROOT/eosio.system/include -o="$ORACLE_ROOT/$ORACLE_WASM" -abigen "$ORACLE_ROOT/$ORACLE_CPP" 
 cleos set contract oracle111111 $ORACLE_ROOT $ORACLE_WASM $ORACLE_ABI -p oracle111111@active
 cleos push action oracle111111 configure '{}' -p oracle111111@active
-cd /home/gg/contracts/delphioracle_bac2/scripts
-node updaterEOS_1.js
+cd /home/gg/contracts/delphioracle/scripts
+node updater.js
 cd /home/gg/contracts/delphioracle_bac2/scripts
 node updaterEOS_2.js
 cd /home/gg/contracts/delphioracle_bac2/scripts
@@ -280,7 +214,7 @@ CONTRACT_WASM="$CONTRACT.wasm"
 CONTRACT_ABI="$CONTRACT.abi"
 CONTRACT_CPP="$CONTRACT.cpp"
 EOSIO_CONTRACTS_ROOT=/home/gg/contracts/eosio.contracts/contracts
-eosio-cpp -abigen -I $CONTRACT_ROOT -I $EOSIO_CONTRACTS_ROOT/eosio.system/include -o "$CONTRACT_ROOT/$CONTRACT_WASM" "$CONTRACT_ROOT/$CONTRACT_CPP" 
+eosio-cpp -contract=$CONTRACT -I=$CONTRACT_ROOT -I=$EOSIO_CONTRACTS_ROOT/eosio.system/include -o="$CONTRACT_ROOT/$CONTRACT_WASM" -abigen "$CONTRACT_ROOT/$CONTRACT_CPP" 
 cleos set contract datapreproc1 $CONTRACT_ROOT $CONTRACT_WASM $CONTRACT_ABI -p datapreproc1@active
 cleos push action datapreproc1 clear '{}' -p datapreproc1@active
 cleos push action datapreproc1 addpair '{"newpair":"eosusd"}' -p datapreproc1@active
