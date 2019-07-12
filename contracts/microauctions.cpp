56119bacaf98f5e4fcb43168fd421459d830bb68
gg <andrew.peter.bryan@gmail.com>
2019-07-12 18:25:42 -0500

    testing vigorairburn

--- contracts/microauctions.cpp
+++ contracts/microauctions.cpp
@@ -236,7 +236,7 @@ CONTRACT microauctions : public eosio::contract {
           savings.contract = current_settings.accepted_token.contract;
           savings.quantity = quantity;
           
-          save(savings);
+         // save(savings);
         }
 
 
@@ -348,7 +348,7 @@ CONTRACT microauctions : public eosio::contract {
 };
 
 extern "C" {
-  [[noreturn]] void apply(uint64_t receiver, uint64_t code, uint64_t action) {
+  void apply(uint64_t receiver, uint64_t code, uint64_t action) {
     if (action == "transfer"_n.value && code != receiver) {
       eosio::execute_action(eosio::name(receiver), eosio::name(code),
                             &microauctions::transfer);
--- scripts/test_microauctions.sh
+++ scripts/test_microauctions.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-# Usage: ./test_microauctions.sh
-
+# Usage: ./test.sh
 #=================================================================================#
 # SETUP
 # 
 pkill nodeos
 rm -rf ~/.local/share/eosio/nodeos/data
-nodeos -e -p eosio --http-validate-host=false --delete-all-blocks --contracts-console --plugin eosio::chain_api_plugin --plugin eosio::history_api_plugin --plugin eosio::producer_plugin --plugin eosio::http_plugin --max-transaction-time=10000
+nodeos -e -p eosio --http-validate-host=false --delete-all-blocks --plugin eosio::chain_api_plugin --contracts-console --plugin eosio::http_plugin --plugin eosio::history_api_plugin --verbose-http-errors --max-transaction-time=10000
+#nodeos -e -p eosio --plugin eosio::producer_plugin --plugin eosio::chain_api_plugin --plugin eosio::http_plugin --plugin eosio::state_history_plugin --access-control-allow-origin='*' --contracts-console --http-validate-host=false --trace-history --chain-state-history --verbose-http-errors --filter-on='*' --disable-replay-opts >> nodeos.log 2>&1 &
+
 
 CYAN='\033[1;36m'
 NC='\033[0m'
 
 # CHANGE PATH
-EOSIO_CONTRACTS_ROOT=/home/ab/contracts1.6.0/eosio.contracts/contracts
-CONTRACT_ROOT=/home/ab/contracts1.6.0/microauctions/contracts
-CONTRACT="microauctions"
-CONTRACT_WASM="$CONTRACT.wasm"
-CONTRACT_ABI="$CONTRACT.abi"
-CONTRACT_CPP="$CONTRACT.cpp"
+EOSIO_CONTRACTS_ROOT=/home/gg/contracts/eosio.contracts/contracts
+
 
 OWNER_KEY="EOS6TnW2MQbZwXHWDHAYQazmdc3Sc1KGv4M9TSgsKZJSo43Uxs2Bx"
 OWNER_ACCT="5J3TQGkkiRQBKcg8Gg2a7Kk5a2QAQXsyGrkCnnq4krSSJSUkW12"
 
 #cleos wallet create --to-console
-cleos wallet unlock -n default --password PW5KDyCJVL3ypUGia4yf5TatcCQ4UjyrDQ296Dh2pe8ZjrLVDPh91
-cleos wallet import -n default --private-key $OWNER_ACCT
+cleos wallet unlock -n default --password PW5HzuR3R2g77WZCsqaSMD91aC3WPFQzmeHkyzyEREbPTB3EmHeMC
+#cleos wallet import -n default --private-key $OWNER_ACCT
 
 #=================================================================================#
 # BOOTSTRAP EOS CRAP
@@ -54,14 +51,20 @@ cleos create account eosio eosio.stake EOS5an8bvYFHZBmiCAzAtVSiEiixbJhLY8Uy5Z7cp
 #cleos create account eosio eosio.token EOS7JPVyejkbQHzE9Z4HwewNzGss11GB21NPkwTX2MQFmruYFqGXm
 cleos create account eosio eosio.token EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
 cleos create account eosio eosio.vpay EOS6szGbnziz224T1JGoUUFu2LynVG72f8D3UVAS25QgwawdH983U
+cleos create account eosio eosio.rex EOS6szGbnziz224T1JGoUUFu2LynVG72f8D3UVAS25QgwawdH983U
+
+
 
 # Bootstrap new system contracts
 echo -e "${CYAN}-----------------------SYSTEM CONTRACTS-----------------------${NC}"
-#eosio-cpp -I $EOSIO_CONTRACTS_ROOT/eosio.msig/include -o $EOSIO_CONTRACTS_ROOT/eosio.msig/eosio.msig.wasm $EOSIO_CONTRACTS_ROOT/eosio.msig/src/eosio.msig.cpp --abigen
-#eosio-cpp -I $EOSIO_CONTRACTS_ROOT/eosio.system/include -I $EOSIO_CONTRACTS_ROOT/eosio.token/include -o $EOSIO_CONTRACTS_ROOT/eosio.system/eosio.system.wasm $EOSIO_CONTRACTS_ROOT/eosio.system/src/eosio.system.cpp --abigen
-#eosio-cpp -I $EOSIO_CONTRACTS_ROOT/eosio.wrap/include -o $EOSIO_CONTRACTS_ROOT/eosio.wrap/eosio.wrap.wasm $EOSIO_CONTRACTS_ROOT/eosio.wrap/src/eosio.wrap.cpp --abigen
+#eosio-cpp -contract=eosio.msig -I=$EOSIO_CONTRACTS_ROOT/eosio.msig/include -o=$EOSIO_CONTRACTS_ROOT/eosio.msig/eosio.msig.wasm -abigen $EOSIO_CONTRACTS_ROOT/eosio.msig/src/eosio.msig.cpp
+#eosio-cpp -contract=eosio.system -I=$EOSIO_CONTRACTS_ROOT/eosio.system/include -I=$EOSIO_CONTRACTS_ROOT/eosio.token/include -o=$EOSIO_CONTRACTS_ROOT/eosio.system/eosio.system.wasm -abigen $EOSIO_CONTRACTS_ROOT/eosio.system/src/eosio.system.cpp
+#eosio-cpp -contract=eosio.wrap -I=$EOSIO_CONTRACTS_ROOT/eosio.wrap/include -o=$EOSIO_CONTRACTS_ROOT/eosio.wrap/eosio.wrap.wasm -abigen $EOSIO_CONTRACTS_ROOT/eosio.wrap/src/eosio.wrap.cpp
+#eosio-cpp -contract=eosio.token -I=$EOSIO_CONTRACTS_ROOT/eosio.token/include -o=$EOSIO_CONTRACTS_ROOT/eosio.token/eosio.token.wasm -abigen $EOSIO_CONTRACTS_ROOT/eosio.token/src/eosio.token.cpp
+#eosio-cpp -contract=eosio.bios -I=$EOSIO_CONTRACTS_ROOT/eosio.bios/include -o=$EOSIO_CONTRACTS_ROOT/eosio.bios/eosio.bios.wasm -abigen $EOSIO_CONTRACTS_ROOT/eosio.bios/src/eosio.bios.cpp
+
 cleos set contract eosio.token $EOSIO_CONTRACTS_ROOT/eosio.token/
-cleos set contract eosio.msig $EOSIO_CONTRACTS_ROOT/eosio.msig/
+cleos set contract eosio.msig $EOSIO_CONTRACTS_ROOT/eosio.msig
 cleos push action eosio.token create '[ "eosio", "100000000000.0000 EOS" ]' -p eosio.token
 cleos push action eosio.token create '[ "eosio", "100000000000.0000 SYS" ]' -p eosio.token
 echo -e "      EOS TOKEN CREATED"
@@ -73,8 +76,8 @@ echo -e "      BIOS SET"
 cleos set contract eosio $EOSIO_CONTRACTS_ROOT/eosio.system/
 echo -e "      SYSTEM SET"
 cleos push action eosio setpriv '["eosio.msig", 1]' -p eosio@active
-cleos push action eosio init '[0, "4,EOS"]' -p eosio@active
-#cleos push action eosio init '[0, "4,SYS"]' -p eosio@active
+cleos push action eosio init '["0", "4,EOS"]' -p eosio@active
+#cleos push action eosio init '["0", "4,SYS"]' -p eosio@active
 
 # Deploy eosio.wrap
 echo -e "${CYAN}-----------------------EOSIO WRAP-----------------------${NC}"
@@ -84,12 +87,20 @@ cleos push action eosio setpriv '["eosio.wrap", 1]' -p eosio@active
 cleos set contract eosio.wrap $EOSIO_CONTRACTS_ROOT/eosio.wrap/
 
 #=================================================================================#
-# create the genesistrack account, set the contract, create UZD stablecoins
+# create the vigorairburn account, set the contract, create UZD stablecoins
 
-cleos system newaccount eosio genesistrack $OWNER_KEY --stake-cpu "50 EOS" --stake-net "10 EOS" --buy-ram-kbytes 50000 --transfer
-cleos set account permission genesistrack active '{"threshold":1,"keys":[{"key":"EOS6TnW2MQbZwXHWDHAYQazmdc3Sc1KGv4M9TSgsKZJSo43Uxs2Bx","weight":1}],"accounts":[{"permission":{"actor":"genesistrack","permission":"eosio.code"},"weight":1}],"waits":[]}' -p genesistrack@active
-eosio-cpp -I $CONTRACT_ROOT -o "$CONTRACT_ROOT/$CONTRACT_WASM" "$CONTRACT_ROOT/$CONTRACT_CPP" --abigen
-cleos set contract genesistrack $CONTRACT_ROOT $CONTRACT_WASM $CONTRACT_ABI -p genesistrack@active
+cleos system newaccount eosio vigorairburn $OWNER_KEY --stake-cpu "50 EOS" --stake-net "10 EOS" --buy-ram-kbytes 50000 --transfer
+cleos set account permission vigorairburn active '{"threshold":1,"keys":[{"key":"EOS6TnW2MQbZwXHWDHAYQazmdc3Sc1KGv4M9TSgsKZJSo43Uxs2Bx","weight":1}],"accounts":[{"permission":{"actor":"vigorairburn","permission":"eosio.code"},"weight":1}],"waits":[]}' -p vigorairburn@active
+
+
+CONTRACT_ROOT=/home/gg/contracts/vigor/contracts
+CONTRACT="microauctions"
+CONTRACT_WASM="$CONTRACT.wasm"
+CONTRACT_ABI="$CONTRACT.abi"
+CONTRACT_CPP="$CONTRACT.cpp"
+EOSIO_CONTRACTS_ROOT=/home/gg/contracts/eosio.contracts/contracts
+#eosio-cpp -contract=$CONTRACT -I=$CONTRACT_ROOT -I=$EOSIO_CONTRACTS_ROOT/eosio.system/include -o="$CONTRACT_ROOT/$CONTRACT_WASM" -abigen "$CONTRACT_ROOT/$CONTRACT_CPP" 
+cleos set contract vigorairburn $CONTRACT_ROOT $CONTRACT_WASM $CONTRACT_ABI -p vigorairburn@active
 
 cleos system newaccount eosio testbuy11111 $OWNER_KEY --stake-cpu "50 EOS" --stake-net "10 EOS" --buy-ram-kbytes 50000 --transfer
 cleos system newaccount eosio testbuy11112 $OWNER_KEY --stake-cpu "50 EOS" --stake-net "10 EOS" --buy-ram-kbytes 50000 --transfer
@@ -115,8 +126,8 @@ cleos push action vig111111111 issue '[ "vig111111111", "1000000000.0000 VIG", "
 #payout_cycles_per_user; // how many cycles aggregated per transfer for a user
  
  echo '{"setting":
-     {"whitelist": "genesistrack",
-                    "tokens_account": "genesistrack",
+     {"whitelist": "vigorairburn",
+                    "tokens_account": "vigorairburn",
                     "savings_account": "testsavings1",
                     "cycles": 3,
                     "seconds_per_cycle": 60,
@@ -134,27 +145,50 @@ cleos push action vig111111111 issue '[ "vig111111111", "1000000000.0000 VIG", "
                     "payout_cycles_per_user": 10}
                     }'> "$CONTRACT_ROOT/settings.json"
 
-cleos push action genesistrack init "$CONTRACT_ROOT/settings.json" -p genesistrack
-cleos push action vig111111111 transfer '[ "vig111111111", "genesistrack", "100000000.0000 VIG", "seed transfer" ]' -p vig111111111@active
+cleos push action vigorairburn init "$CONTRACT_ROOT/settings.json" -p vigorairburn
+
+echo '{
+    "threshold": 1,
+    "keys": [],
+    "accounts": [
+        {"permission":{"actor":"eosio", "permission":"active"}, "weight":1},
+        {"permission":{"actor":"vigorairburn", "permission":"eosio.code"}, "weight":1}
+    ],
+    "waits": []
+}' > airburn_active.json
+cleos set account permission vigorairburn active ./airburn_active.json '' -p vigorairburn@active
+
+echo '{
+    "threshold": 1,
+    "keys": [],
+    "accounts": [
+        {"permission":{"actor":"eosio", "permission":"active"}, "weight":1},
+    ],
+    "waits": []
+}' > airburn_owner.json
+cleos set account permission vigorairburn owner ./airburn_owner.json '' -p vigorairburn@owner
+
+cleos push action vig111111111 transfer '[ "vig111111111", "vigorairburn", "100000000.0000 VIG", "seed transfer" ]' -p vig111111111@active
 
 #buy from 0th cycle
-cleos push action eosio.token transfer '[ "testbuy11111", "genesistrack", "100.0000 EOS", "" ]' -p testbuy11111@active
-cleos push action eosio.token transfer '[ "testbuy11112", "genesistrack", "200.0000 EOS", "" ]' -p testbuy11112@active
-cleos push action genesistrack claim '[ "testbuy11111"]' -p testbuy11111@active
-cleos push action genesistrack claim '[ "testbuy11112"]' -p testbuy11111@active
+cleos push action eosio.token transfer '[ "testbuy11111", "vigorairburn", "100.0000 EOS", "" ]' -p testbuy11111@active
+cleos push action eosio.token transfer '[ "testbuy11112", "vigorairburn", "200.0000 EOS", "" ]' -p testbuy11112@active
+cleos push action vigorairburn claim '[ "testbuy11111"]' -p testbuy11111@active
+cleos push action vigorairburn claim '[ "testbuy11112"]' -p testbuy11111@active
 
 #pause 60 seconds
 #buy from 1st cycle
-cleos push action eosio.token transfer '[ "testbuy11111", "genesistrack", "100.0000 EOS", "" ]' -p testbuy11111@active
-cleos push action eosio.token transfer '[ "testbuy11112", "genesistrack", "200.0000 EOS", "" ]' -p testbuy11112@active
-cleos push action genesistrack claim '[ "testbuy11111"]' -p testbuy11111@active
-cleos push action genesistrack claim '[ "testbuy11112"]' -p testbuy11111@active
+cleos push action eosio.token transfer '[ "testbuy11111", "vigorairburn", "100.0000 EOS", "" ]' -p testbuy11111@active
+cleos push action eosio.token transfer '[ "testbuy11112", "vigorairburn", "200.0000 EOS", "" ]' -p testbuy11112@active
+cleos push action vigorairburn claim '[ "testbuy11111"]' -p testbuy11111@active
+cleos push action vigorairburn claim '[ "testbuy11112"]' -p testbuy11111@active
 
 #check balances of all users, and sale proceeds were sent to savings
-cleos get table genesistrack genesistrack payment
-cleos get table genesistrack genesistrack cycle
-cleos get table vig111111111 genesistrack accounts
+cleos get table vigorairburn vigorairburn payment
+cleos get table vigorairburn vigorairburn cycle
+cleos get table vig111111111 vigorairburn accounts
 cleos get table vig111111111 vig111111111 accounts
 cleos get table vig111111111 testbuy11111 accounts
 cleos get table vig111111111 testbuy11112 accounts
 cleos get table eosio.token testsavings1 accounts
+cleos get table eosio.token vigorairburn accounts
